2489013c8273bfea2864d3a48f58a85f54ba62bd
JermyJerm <99265510+JermyJerm@users.noreply.github.com>
2022-05-12 08:29:45 -0400

    l

--- Assets/Script/NonPlayerCharacter.cs
+++ Assets/Script/NonPlayerCharacter.cs
@@ -10,27 +10,26 @@ float timerDisplay;
     // Start is called before the first frame update
     void Start()
     {
-        
+        dialogBox.SetActive(false);
+        timerDisplay = -1.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        dialogBox.SetActive(false);
-        timerDisplay = -1.0f;
 
         if (timerDisplay >= 0)
-    {
-        timerDisplay -= Time.deltaTime;
-        if (timerDisplay < 0)
         {
-            dialogBox.SetActive(false);
-        }
+            timerDisplay -= Time.deltaTime;
+            if (timerDisplay < 0)
+            {
+                dialogBox.SetActive(false);
+            }
     }
     }
     public void DisplayDialog()
-{
-    timerDisplay = displayTime;
-    dialogBox.SetActive(true);
-}
+    {
+        timerDisplay = displayTime;
+        dialogBox.SetActive(true);
+    }
 }
